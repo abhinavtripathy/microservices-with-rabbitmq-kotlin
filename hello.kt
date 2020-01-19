0cc1b47d106ee0fadf3ea3a2e430ed1eabe91042
abhinavtripathy <abhinavtripathy99@gmail.com>
2020-01-19 18:14:34 +0530

    minor

--- hello.kt
+++ hello.kt
@@ -1,3 +1,29 @@
 fun main() {
-    println("hello world")
+    println(arr())
+}
+
+fun loop() {
+
+    for(i in 1..3) {
+
+        println("hello world")
+
+    }
+
+    return "done"
+
+}
+
+fun arr(): int {
+
+    val arr = IntArray(5);
+
+    for(i in arr.indices) {
+
+        arr[i] = i;
+
+    }
+
+    return arr
+
 }
